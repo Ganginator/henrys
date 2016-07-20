6ebdf23d2c73c0b50cd18cc70f164eb1f3e47c10
Ganginator <jgangi@aerialapps.com>
2016-07-20 11:36:08 -0500

    development

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -93,7 +93,44 @@ if ( $order ) : ?>
             
         </div><!-- .medium-10-->
     </div><!--	.row	-->
+    
+    <!-- Facebook Conversion Code for Purchase Complete - HK -->
+	<script>(function() {
+	var _fbq = window._fbq || (window._fbq = []);
+	if (!_fbq.loaded) {
+	var fbds = document.createElement('script');
+	fbds.async = true;
+	fbds.src = '//connect.facebook.net/en_US/fbds.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(fbds, s);
+	_fbq.loaded = true;
+	}
+	})();
+	window._fbq = window._fbq || [];
+	window._fbq.push(['track', '6029002441635', {'value':'0.00','currency':'USD'}]);
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6029002441635&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
 	
+	<!-- Google Code for Purchase Complete Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 964514327;
+	var google_conversion_language = "en";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "5EpUCIS0_l0Ql6T1ywM";
+	var google_remarketing_only = false;
+	/* ]]> */
+	</script>
+	<script type="text/javascript"
+	src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+	<img height="1" width="1" style="border-style:none;" alt=""
+	src="//www.googleadservices.com/pagead/conversion/964514327/?label=5EpUCIS0_l0Ql6T1ywM&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
 
 <?php else : ?>
 	<div class="row">
